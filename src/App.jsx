c5ef24a03ba4f4e10aa81f5e18b6071f59723aac
AhuraMagda <lubaszka.magda@gmail.com>
2023-08-25 18:05:48 +0200

    scrimba project

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import './App.css'
+import Die from './components/Die'
+import React from 'react';
+import Confetti from 'react-confetti'
+
+function App() {
+  const allNewDice = () => {
+    const randomDiceArray = [];
+    for (let i = 0; i<10; i++) {
+      randomDiceArray.push({id: i, value: Math.ceil(Math.random() * 6), isHeld: false})
+    }
+    return randomDiceArray
+  }
+
+  const [dice, setDice] = React.useState(allNewDice())
+
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={()=>holdDice(die.id)}/>)
+
+  function rollDice() {
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6)}
+    }))
+  }
+
+  function startNewGame() {
+    setDice(allNewDice())
+    setTenzies(false)
+  }
+
+  function holdDice(id) {
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ? {...die, isHeld: !die.isHeld} : die
+    }))
+  }
+
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(()=> {
+    const allHeld = dice.every(die => die.isHeld)
+    const allSame = dice.every(die => die.value === dice[0].value)
+    allHeld && allSame && setTenzies(true)
+  }, [dice])
+
+
+  return (
+    <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className='dies-container'>
+        {diceElements}
+      </div>
+      <button onClick={tenzies ? startNewGame : rollDice} className='game-button'>{tenzies ? "New Game " : "Roll"}</button>
+
+    </main> 
+  )
+}
+
+export default App
---
+++ src/components/Die.jsx
@@ -0,0 +1,16 @@
+import './Die.css'
+
+
+
+function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF"
+    }
+    return (
+        <div className="die" style={styles} onClick={props.holdDice}>
+            <h2>{props.value}</h2>
+        </div>
+    )
+}
+
+export default Die
