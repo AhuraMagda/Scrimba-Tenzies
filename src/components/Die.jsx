1cb67350fef3cefa47bd5b29ef8d3cdb06c22e4e
AhuraMagda <lubaszka.magda@gmail.com>
2023-11-04 09:29:08 +0100

    cleanup

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,16 +1,14 @@
-import './Die.css'
-
-
+import "./Die.css";
 
 function Die(props) {
-    const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF"
-    }
-    return (
-        <div className="die" style={styles} onClick={props.holdDice}>
-            <h2>{props.value}</h2>
-        </div>
-    )
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF",
+  };
+  return (
+    <div className="die" style={styles} onClick={props.holdDice}>
+      <h2>{props.value}</h2>
+    </div>
+  );
 }
 
-export default Die
+export default Die;
